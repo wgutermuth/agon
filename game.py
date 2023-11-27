1c461a7e47be839780740170c8ec69cc28e406aa
wgutermuth <147719695+wgutermuth@users.noreply.github.com>
2023-11-27 09:24:23 -0500

    Play touches land they die.

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ from settings import *
 from background import Background
 from cannon import CannonBall
 from positions import *
+from obstacle import Obstacle
 import math
 
 pygame.init()
@@ -13,20 +14,19 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 cannon_balls = pygame.sprite.Group()
+obstacles = pygame.sprite.Group()
+obstacles.add([Obstacle(220,0,80,540)])
+obstacles.add([Obstacle(257,80,43,465)])
+obstacles.add([Obstacle(300,123,45,425)])
+obstacles.add([Obstacle(386,168,45,290)])
+obstacles.add([Obstacle(386,213,40,210)])
+obstacles.add([Obstacle(474,253,40,75)])
+obstacles.add([Obstacle(732,517,400,460)])
+
 
 pygame.display.set_caption("Shipwreck Showdown")
 
 background = Background(map="1")
-
-# Define rectangle coordinates
-start_point = (215, 0)
-end_point = (772, 78)
-
-# Create a sprite with a rectangle
-rectangle_sprite = pygame.sprite.Sprite()
-rectangle_sprite.image = pygame.Surface((end_point[0] - start_point[0], end_point[1] - start_point[1]))
-rectangle_sprite.image.fill((255, 255, 255))  # Fill with white color
-rectangle_sprite.rect = rectangle_sprite.image.get_rect(topleft=start_point)
 
 player1 = Player()
 
@@ -57,7 +57,7 @@ while True:
             if event.key == pygame.K_DOWN:
                 player1.moving_down = False
             if event.key == pygame.K_SPACE:
-                player_fire_up = CannonBall(player1.rect.centerx, player1.rect.y-10, direction="up", speed=2)
+                player_fire_up = CannonBall(player1.rect.centerx, player1.rect.y - 10, direction="up", speed=2)
                 cannon_balls.add(player_fire_up)
 
                 player_fire_down = CannonBall(player1.rect.centerx, player1.rect.bottom, direction="down", speed=2)
@@ -81,16 +81,21 @@ while True:
         new_cannon_ball = CannonBall(*UCANNON_MAP1_1, direction="up", speed=2)
         cannon_balls.add(new_cannon_ball)
         cannonball_timer = current_time  # Reset the timer
-    # update game objects
+
+    # Update game objects
     cannon_balls.update()
     player1.update()
 
-    # draw the game screen
+    # Draw the game screen
     background.draw(screen)
     player1.draw(screen)
     cannon_balls.draw(screen)
 
-    player_hit_list = spritecollide(player1, cannon_balls, True)  # The third parameter (True) removes the cannonball on collision
+    player_hit_list = spritecollide(player1, cannon_balls, True)
+    if player_hit_list:
+        player1.die()
+
+    player_hit_list = spritecollide(player1, obstacles, False)
     if player_hit_list:
         player1.die()
 
---
+++ obstacle.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.sprite import Sprite
+from pygame.locals import Rect
+from settings import TILE_SIZE
+
+class Obstacle(Sprite):
+    def __init__(self, x, y, height, width):
+        super().__init__()
+
+        self.rect = pygame.rect.Rect(x,y,width,height)
+
+
+    def draw(self, screen):
+        pygame.draw.rect(screen,(255,255,255),self.rect)
