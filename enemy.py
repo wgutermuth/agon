dfdfe8e4c0ef89c7c3f636ff397355ad3248ca24
wgutermuth <147719695+wgutermuth@users.noreply.github.com>
2023-11-29 09:45:22 -0500

    Enemies and "enemies."

--- enemy.py
+++ enemy.py
@@ -19,18 +19,45 @@ class Enemy(pygame.sprite.Sprite):
         self.bounds = bounds
         self.speed_x = speed_x
         self.speed_y = speed_y
+        self.hit_counter = 0
 
     def update(self):
         if self.direction == "horizontal":
             self.rect.x += self.speed_x
             if self.rect.left < self.bounds[0][0] or self.rect.right > self.bounds[1][0]:
-                self.speed_x = -self.speed_x  # Reverse the horizontal direction
+                self.speed_x = -self.speed_x
                 self.image = self.left_image if self.image == self.right_image else self.right_image
         elif self.direction == "vertical":
             self.rect.y += self.speed_y
             if self.rect.top < self.bounds[0][1] or self.rect.bottom > self.bounds[1][1]:
-                self.speed_y = -self.speed_y  # Reverse the vertical direction
+                self.speed_y = -self.speed_y
                 self.image = pygame.transform.flip(self.image, False, True)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def die(self):
+        self.hit_counter += 1
+
+        if self.hit_counter == 1:
+            self.player_image = pygame.image.load(f"assets/images/Ships/{color}_ship_damaged2.png").convert_alpha()
+            self.scaled_image = pygame.transform.scale(self.player_image, (
+            int(self.player_image.get_width() * SHIP_SCALING_FACTOR),
+            int(self.player_image.get_height() * SHIP_SCALING_FACTOR)))
+            self.right_image = pygame.transform.rotate(self.scaled_image, 90)
+            self.left_image = pygame.transform.flip(self.right_image, True, False)
+            if self.image == self.right_image:
+                self.image = self.right_image
+            else:
+                self.image = self.left_image
+        elif self.hit_counter == 2:
+            self.player_image = pygame.image.load(f"assets/images/Ships/{color}_ship_destroyed.png").convert_alpha()
+            self.scaled_image = pygame.transform.scale(self.player_image, (
+            int(self.player_image.get_width() * SHIP_SCALING_FACTOR),
+            int(self.player_image.get_height() * SHIP_SCALING_FACTOR)))
+            self.right_image = pygame.transform.rotate(self.scaled_image, 90)
+            self.left_image = pygame.transform.flip(self.right_image, True, False)
+            if self.image == self.right_image:
+                self.image = self.right_image
+            else:
+                self.image = self.left_image
--- testing.py
+++ testing.py
@@ -15,6 +15,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 cannon_balls = pygame.sprite.Group()
+player_cannon_balls = pygame.sprite.Group()
 obstacles = pygame.sprite.Group()
 obstacles.add([Obstacle(220,0,80,540)])
 obstacles.add([Obstacle(257,80,43,465)])
@@ -32,8 +33,13 @@ background = Background(map="1")
 player1 = Player()
 
 enemy_blue = Enemy("blue", 100, 200, "horizontal", [(10, 200), (380, 300)])
+enemy_green = Enemy("green", 966, 270, "vertical", [(966, 6), (966, 503)])
+enemy_red = Enemy("red", 1119, 400, "vertical", [(1119, 6), (1119, 503)])
+enemy_yellow = Enemy("yellow", 492, 601, "horizontal", [(3, 601), (713, 601)])
+enemies = pygame.sprite.Group([enemy_blue,enemy_green,enemy_red,enemy_yellow])
 
 cannonball_timer = pygame.time.get_ticks()
+enemy_cannonball_timer = pygame.time.get_ticks()
 
 while True:
     for event in pygame.event.get():
@@ -61,10 +67,10 @@ while True:
                 player1.moving_down = False
             if event.key == pygame.K_SPACE:
                 player_fire_up = CannonBall(player1.rect.centerx, player1.rect.y - 10, direction="up", speed=2)
-                cannon_balls.add(player_fire_up)
+                player_cannon_balls.add(player_fire_up)
 
                 player_fire_down = CannonBall(player1.rect.centerx, player1.rect.bottom, direction="down", speed=2)
-                cannon_balls.add(player_fire_down)
+                player_cannon_balls.add(player_fire_down)
 
     # Check if it's time to create a new cannonball
     current_time = pygame.time.get_ticks()
@@ -85,20 +91,48 @@ while True:
         cannon_balls.add(new_cannon_ball)
         cannonball_timer = current_time  # Reset the timer
 
+    enemy_current_time = pygame.time.get_ticks()
+    if enemy_current_time - enemy_cannonball_timer >= ENEMY_CANNON_INTERVAL:
+        new_cannon_ball = CannonBall(enemy_blue.rect.centerx, enemy_blue.rect.bottom, direction="down", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        new_cannon_ball = CannonBall(enemy_blue.rect.centerx, enemy_blue.rect.y - 10, direction="up", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        # Vertical
+        new_cannon_ball = CannonBall(enemy_red.rect.left, enemy_red.rect.centery, direction="left", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        new_cannon_ball = CannonBall(enemy_red.rect.right, enemy_red.rect.centery, direction="right", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        # Vertical
+        new_cannon_ball = CannonBall(enemy_green.rect.left, enemy_green.rect.centery, direction="left", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        new_cannon_ball = CannonBall(enemy_green.rect.right, enemy_green.rect.centery, direction="right", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        new_cannon_ball = CannonBall(enemy_yellow.rect.centerx, enemy_yellow.rect.bottom, direction="down", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        new_cannon_ball = CannonBall(enemy_yellow.rect.centerx, enemy_yellow.rect.y - 10, direction="up", speed=2)
+        cannon_balls.add(new_cannon_ball)
+        enemy_cannonball_timer = enemy_current_time
+
     # Update game objects
     cannon_balls.update()
+    player_cannon_balls.update()
     player1.update()
-    enemy_blue.update()
+    enemies.update()
+    player_cannon_balls.update()
 
     # Draw the game screen
     background.draw(screen)
     player1.draw(screen)
     cannon_balls.draw(screen)
-    enemy_blue.draw(screen)
+    enemies.draw(screen)
 
     player_hit_list = spritecollide(player1, cannon_balls, True)
     if player_hit_list:
         player1.die()
+
+    enemy_red_hit_list = pygame.sprite.groupcollide(enemies, player_cannon_balls, True,True)
+    if enemy_red_hit_list:
+        enemies.die()
 
     player_hit_list = spritecollide(player1, obstacles, False)
     if player_hit_list:
